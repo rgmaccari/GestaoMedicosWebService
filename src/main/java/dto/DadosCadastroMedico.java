33f2639bec0694f5a1d6fdf56dfc6b33e7a53aa2
Rafael Maccari <rafagmaccari@gmail.com>
2025-04-06 13:21:23 -0300

    creating DadosCadastroMedico

--- src/main/java/dto/DadosCadastroMedico.java
+++ src/main/java/dto/DadosCadastroMedico.java
@@ -1,9 +1,10 @@
-package Model;
+package dto;
 
-import dto.DadosCadastroMedico;
+import Model.Endereco;
+import Model.Especialidade;
+import Model.Paciente;
 
-public class Medico {
-    private int id;
+public class DadosCadastroMedico {
     private String nome;
     private String email;
     private String telefone;
@@ -11,27 +12,6 @@ public class Medico {
     private Especialidade especialidade;
     private Endereco endereco;
     private boolean ativo;
-
-    public Medico() {
-    }
-
-    public Medico(DadosCadastroMedico dadosCadastroMedico) {
-        this.ativo = true;
-        this.nome = dadosCadastroMedico.getNome();
-        this.email = dadosCadastroMedico.getEmail();
-        this.telefone = dadosCadastroMedico.getTelefone();
-        this.crm = dadosCadastroMedico.getCrm();
-        this.especialidade = dadosCadastroMedico.getEspecialidade();
-        this.endereco = dadosCadastroMedico.getEndereco();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
@@ -91,15 +71,11 @@ public class Medico {
 }
 
 /**
- * RQF-001 Cadastro de médicos
- * O sistema deve possuir uma funcionalidade de cadastro de médicos, na qual as
- * seguintes informações deverão ser preenchidas:
- * Nome
- * E-mail
- * Telefone
- * CRM
- * Especialidade (Ortopedia, Cardiologia, Ginecologia ou Dermatologia)
- * Endereço completo (logradouro, número, complemento, bairro)
- * Todas as informações são de preenchimento obrigatório, exceto o número e o
- * complemento do endereço.
- * */
+ * private int id;
+ *     private String nome;
+ *     private String email;
+ *     private String telefone;
+ *     private String crm;
+ *     private Especialidade especialidade;
+ *     private Endereco endereco;
+ *     private boolean ativo;*/
